bd71aa5f6620ea4f2fcb09c5db2a54bc4c3bcbed
Marshysaurus <ryuzaki.orozco@gmail.com>
2020-09-13 19:29:12 -0500

    .env file configuration

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,12 +2,12 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCQBdUbTiRwVVlwNzFk8qI06QsnHBWiOTo',
-  authDomain: 'citadels-ldaw.firebaseapp.com',
-  databaseURL: 'https://citadels-ldaw.firebaseio.com',
-  projectId: 'citadels-ldaw',
-  storageBucket: 'citadels-ldaw.appspot.com',
-  messagingSenderId: '82097961134',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: '1:82097961134:web:dd88b4d82a6e50d23a953f',
   measurementId: 'G-9532JM468P',
 };
