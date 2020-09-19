91e4955ade5c6feeff117ae3e46967ce49a33d5c
Kai Kawasaki Ueda <kaiu8x@Kais-MacBook-Pro.local>
2020-09-18 20:00:11 -0500

    fix

--- src/App.react.js
+++ src/App.react.js
@@ -5,7 +5,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Landing from 'components/landing/Landing.react';
 import Home from 'components/home/Home.react';
 import Authentication from 'components/authentication/Authentication.react';
-import Home from 'components/home/Home.react';
 
 import AuthRenderer from 'components/shared/AuthRenderer.react';
 import * as ROUTES from 'constants/routes';
