765f81b18329f5469a367659c3bf0e3f00aa781e
Kai Kawasaki Ueda <kaiu8x@Kais-MacBook-Pro.local>
2020-09-18 22:21:59 -0500

    create game forms

---
+++ src/components/formDialogButton/FormDialogButton.react.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CreateGameDialog from 'components/formDialogButton/CreateGameDialog.react';
+
+function FormDialogButton() {
+    const [open, setOpen] = React.useState(false);
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    return (
+      <div>
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          Create Game
+        </Button>
+        <CreateGameDialog open={open} onClose={handleClose}/>
+      </div>
+    );
+  }
+
+  export default FormDialogButton;
