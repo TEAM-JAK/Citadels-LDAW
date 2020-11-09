02aa4271b5e095b02373f9e640bfd13a9a5f37a4
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-11-08 18:14:26 -0600

    Added env variables to firebase config

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -10,8 +10,8 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: '1:82097961134:web:dd88b4d82a6e50d23a953f',
-  measurementId: 'G-9532JM468P',
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 class Firebase {
