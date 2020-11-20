ff885fe44e5af1261a3dfaf4b77801c32062e0d3
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-11-19 22:17:43 -0600

    Serving static files

--- game-server/index.js
+++ game-server/index.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const admin = require('firebase-admin');
 const {Server} = require('boardgame.io/server');
 const {Firestore} = require('bgio-firebase');
+const {historyApiFallback} = require('koa2-connect-history-api-fallback');
+const serve = require('koa-static');
 const CitadelsGame = require('../src/game/CitadelsGame');
+const {DEFAULT_PORT} = require('../src/game/config');
 const serviceAccount = require('./firebaseServiceAccountKey.json');
 
 const database = new Firestore({
@@ -12,9 +16,21 @@ const database = new Firestore({
   dbPrefix: 'bgio_',
 });
 
+const root = path.join(__dirname, '../build');
+
 const server = Server({
   games: [CitadelsGame.default],
   db: database,
 });
 
-server.run(8000);
+// Solves the refresh problem when using History API
+server.app.use(
+  historyApiFallback({
+    index: 'index.html',
+    whiteList: ['/api', '/games'],
+  }),
+);
+// Serves the React App
+server.app.use(serve(root));
+
+server.run(Number(process.env.PORT || DEFAULT_PORT));
--- src/game/config/client.js
+++ src/game/config/client.js
@@ -1,4 +1,6 @@
-import {DEFAULT_PORT} from '.';
+import {isProduction, DEFAULT_PORT} from '.';
 
-const {protocol, hostname} = window.location;
-export const BGIO_SERVER_URL = `${protocol}//${hostname}:${DEFAULT_PORT}`;
+const {origin, protocol, hostname} = window.location;
+export const BGIO_SERVER_URL = isProduction
+  ? origin
+  : `${protocol}//${hostname}:${DEFAULT_PORT}`;
