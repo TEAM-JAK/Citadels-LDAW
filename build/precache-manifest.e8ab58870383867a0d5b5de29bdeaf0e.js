e4346d87a3af2b24f9845c0083bbcf003e224ea5
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-11-20 20:35:15 -0600

    Added production build

---
+++ build/precache-manifest.e8ab58870383867a0d5b5de29bdeaf0e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114ca61429008fd4d6ea5be2711ff3ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6acdfd332facbef5a09d",
+    "url": "/static/css/2.bacdb943.chunk.css"
+  },
+  {
+    "revision": "4f5e2cab134924e47497",
+    "url": "/static/css/main.9cc76b6e.chunk.css"
+  },
+  {
+    "revision": "6acdfd332facbef5a09d",
+    "url": "/static/js/2.9e7fca22.chunk.js"
+  },
+  {
+    "revision": "e3f51220f80e99971c636a38928521b6",
+    "url": "/static/js/2.9e7fca22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5e2cab134924e47497",
+    "url": "/static/js/main.aa0ae5e4.chunk.js"
+  },
+  {
+    "revision": "df6945ad7b722e0c0294",
+    "url": "/static/js/runtime-main.666ea094.js"
+  },
+  {
+    "revision": "2dbeb8d854e77c952cb636cb14a151c4",
+    "url": "/static/media/nunito-latin-200.2dbeb8d8.woff2"
+  },
+  {
+    "revision": "e53a21a8bf1b97fb9900e8d5c5bfbc14",
+    "url": "/static/media/nunito-latin-200.e53a21a8.woff"
+  },
+  {
+    "revision": "2bcb70df023a72cb0a5864c993d03bfc",
+    "url": "/static/media/nunito-latin-200italic.2bcb70df.woff"
+  },
+  {
+    "revision": "bbdf1c37c56e1bfaf618b9a4ee46d508",
+    "url": "/static/media/nunito-latin-200italic.bbdf1c37.woff2"
+  },
+  {
+    "revision": "97e0d2fc0422400a6e7be8f1b5e4949e",
+    "url": "/static/media/nunito-latin-300.97e0d2fc.woff"
+  },
+  {
+    "revision": "98aabf9aea1a55e2390109ad1efddce3",
+    "url": "/static/media/nunito-latin-300.98aabf9a.woff2"
+  },
+  {
+    "revision": "8646bd832f738c016b56b146af6fcd46",
+    "url": "/static/media/nunito-latin-300italic.8646bd83.woff"
+  },
+  {
+    "revision": "ac71e7f9cd408dd512f6a6bbbd64eb51",
+    "url": "/static/media/nunito-latin-300italic.ac71e7f9.woff2"
+  },
+  {
+    "revision": "508e414e3d3bc41666826fee46c7d881",
+    "url": "/static/media/nunito-latin-400.508e414e.woff2"
+  },
+  {
+    "revision": "e5875b853d135f2a82ceae7ac537b6f4",
+    "url": "/static/media/nunito-latin-400.e5875b85.woff"
+  },
+  {
+    "revision": "09ce3100c075ee04f293b9191c88f156",
+    "url": "/static/media/nunito-latin-400italic.09ce3100.woff"
+  },
+  {
+    "revision": "28f555d9a5c83842f03988ed14f00ef5",
+    "url": "/static/media/nunito-latin-400italic.28f555d9.woff2"
+  },
+  {
+    "revision": "8b8871e482a76d7e9327b02131564af7",
+    "url": "/static/media/nunito-latin-600.8b8871e4.woff2"
+  },
+  {
+    "revision": "f894e279899f0cda9d06e32b78783399",
+    "url": "/static/media/nunito-latin-600.f894e279.woff"
+  },
+  {
+    "revision": "9d150271cf30aefb8609dff4c9ece978",
+    "url": "/static/media/nunito-latin-600italic.9d150271.woff"
+  },
+  {
+    "revision": "9f930640316b0b9e8b088958d8f7aa5c",
+    "url": "/static/media/nunito-latin-600italic.9f930640.woff2"
+  },
+  {
+    "revision": "7d90308f9bdf7321be5e28d017a5ade5",
+    "url": "/static/media/nunito-latin-700.7d90308f.woff"
+  },
+  {
+    "revision": "a22acb48f45d03d672bf5b9389363a83",
+    "url": "/static/media/nunito-latin-700.a22acb48.woff2"
+  },
+  {
+    "revision": "26556875512fee1b823bc6117c076358",
+    "url": "/static/media/nunito-latin-700italic.26556875.woff"
+  },
+  {
+    "revision": "4507a1361620a66827e217855c9fff24",
+    "url": "/static/media/nunito-latin-700italic.4507a136.woff2"
+  },
+  {
+    "revision": "5fa815a069b40c78f26863eaabcd6d2f",
+    "url": "/static/media/nunito-latin-800.5fa815a0.woff"
+  },
+  {
+    "revision": "eaa946756e91563b4b7d766de2f5b7ed",
+    "url": "/static/media/nunito-latin-800.eaa94675.woff2"
+  },
+  {
+    "revision": "2e1a4979ec23066532d157ab5fe4ccc5",
+    "url": "/static/media/nunito-latin-800italic.2e1a4979.woff"
+  },
+  {
+    "revision": "b9cd56a25c86d089cc18bc92dbad167a",
+    "url": "/static/media/nunito-latin-800italic.b9cd56a2.woff2"
+  },
+  {
+    "revision": "19de37363cebfe020de6d4abdbb38c2e",
+    "url": "/static/media/nunito-latin-900.19de3736.woff"
+  },
+  {
+    "revision": "f82c6fb49774f4e4caa570640e164320",
+    "url": "/static/media/nunito-latin-900.f82c6fb4.woff2"
+  },
+  {
+    "revision": "6f10930c04eec040ac4ffb4890191149",
+    "url": "/static/media/nunito-latin-900italic.6f10930c.woff2"
+  },
+  {
+    "revision": "ca56cb53793f0f9084f26315012cefd8",
+    "url": "/static/media/nunito-latin-900italic.ca56cb53.woff"
+  }
+]);
